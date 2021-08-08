a8d42c94bd7ad1c12a9b17b5b2c96ea080ee1b8a
taki <chainmaker85@gmail.com>
2021-08-08 04:50:40 +0000

    first commit

---
+++ mentor.rb
@@ -0,0 +1,30 @@
+class Mentor
+    attr_accessor :name
+    
+    def initialize(name)
+        self.name = name
+    end
+    
+    def job
+        puts "#{self.name}です。私は現役のITプロフェッショナルです。"
+    end
+end
+
+class RailsMentor < Mentor
+    
+    def initialize(name)
+        super(name)
+    end
+    
+    def job 
+        puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
+    end
+    
+end
+
+kirameki = Mentor.new('煌木')
+akade = RailsMentor.new('赤出')
+
+kirameki.job
+akade.job
+
